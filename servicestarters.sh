243f11c7dd7133b7b743e298a1e193f36910c3fd
Vera Balagula <verabalagula@MacBook-Pro-3.domain>
2018-03-03 18:54:26 -0500

    dev work

--- servicestarters.sh
+++ servicestarters.sh
@@ -1,8 +1,8 @@
 cd journai-authentication && npm start && cd ../
-cd journai-frontend && npm start && cd ../
-cd journai-entries && npm start && cd ../
-cd journai-reporting && npm start && cd ../
-cd journai-reminders && npm start && cd ../../
+# cd journai-frontend && npm start && cd ../
+# cd journai-entries && npm start && cd ../
+# cd journai-reporting && npm start && cd ../
+# cd journai-reminders && npm start && cd ../../
 
 printf "\n\nReady for development :)\n\n" 
 
