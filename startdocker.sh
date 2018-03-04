75c56e2402257f955641fdc732effc7bcb6caaa8
Vera Balagula <verabalagula@MacBook-Pro-3.domain>
2018-03-04 00:56:05 -0500

    dev work

--- startdocker.sh
+++ startdocker.sh
@@ -8,28 +8,29 @@ fi
 cd $DIRECTORY 
 declare -a REPOS=("journai-authentication" "journai-entries" "journai-frontend" "journai-reminders" "journai-reporting" "journai-development")
 declare -a PORTS=("1000" "2000" "3000" "4000" "5000")
-# for i in "${!PORTS[@]}"
-# do
-#     SERVICE=${REPOS[i]}
-#     PORT=${PORTS[i]}
-#     docker rm $(docker stop $(docker ps -a -q --filter ancestor=$SERVICE --format="{{.ID}}"))
-#     if [ -d $SERVICE ]; then 
-#         cd $SERVICE
-#         rm -r logs
-#         mkdir logs
-#         git pull development
-#         git checkout -b development
-#         cd ../
-#     else
-#         URL="https://journai:noway123@github.com/journai/$SERVICE.git"
-#         git clone $URL
-#     fi
-#     cd journai-development
-#     docker build --build-arg port=$PORT --build-arg service=$SERVICE . --tag $SERVICE
-#     docker run -p $PORT:$PORT -td -i --mount type=bind,src=$(cd "$(dirname "$0")"; pwd)/../$SERVICE,dst=/home/Journai/$SERVICE $SERVICE
-#     cd ../
-# done
-# printf "\n\nReady for development :)\n\n" 
+for i in "${!PORTS[@]}"
+do
+    SERVICE=${REPOS[i]}
+    PORT=${PORTS[i]}
+    docker rm $(docker stop $(docker ps -a -q --filter ancestor=$SERVICE --format="{{.ID}}"))
+    if [ -d $SERVICE ]; then 
+        cd $SERVICE
+        rm -r logs
+        mkdir logs
+        git pull development
+        git checkout -b development
+        cd ../
+    else
+        URL="https://journai:noway123@github.com/journai/$SERVICE.git"
+        git clone $URL
+    fi
+    cd journai-development
+    docker build --build-arg port=$PORT --build-arg service=$SERVICE . --tag $SERVICE
+    docker run -p $PORT:$PORT -td -i --mount type=bind,src=$(cd "$(dirname "$0")"; pwd)/../$SERVICE,dst=/home/Journai/$SERVICE $SERVICE
+    cd ../
+done
+printf "\n\nReady for development :)\n\n" 
+read -p "Press 'enter' to commit changes"
 for i in "${REPOS[@]}"
 do
     if [ -d $i ]; then 
@@ -42,4 +43,4 @@ do
     fi
 done
 
-# printf "\n\nDone with development :)\n\n" 
+printf "\n\nDone with development :)\n\n" 
